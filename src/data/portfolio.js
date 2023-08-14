69f610bb7e79fe9760c01793c55efcdaa5f5b2ba
Shaun <scpour@gmail.com>
2023-08-13 23:22:03 -0400

    image update

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,25 +1,25 @@
 export default [
    {
       title: 'FreeCodeCamp Drum Machine',
-      imgUrl: "/assets/react-drum-machine.png",
+      imgUrl: "assets/react-drum-machine.png",
       stack: ['HTML', 'CSS', 'JavaScript', 'ReactJS'],
       link: 'https://darkphoenixninja92.github.io/fcc-drum-machine/',
    },
    {
       title: 'Advice Generator Web Application',
-      imgUrl: '/assets/advice.jpg',
+      imgUrl: 'assets/advice.jpg',
       stack: ['HTML', 'CSS', 'JavaScript', 'API'],
       link: 'https://darkphoenixninja92.github.io/advice-generator-app/'
    },
    {
       title: 'Data Pagination List',
-      imgUrl: '/assets/pagination.jpg',
+      imgUrl: 'assets/pagination.jpg',
       stack: ['HTML', 'CSS', 'JavaScript'],
       link: 'https://darkphoenixninja92.github.io/TeamTreeHouse-FullStack-Unit-02-Pagination/',
    },
    {
       title: 'Skilled E-Learning Landing Page',
-      imgUrl: '/assets/skilled-elearning.webp',
+      imgUrl: 'assets/skilled-elearning.webp',
       stack: ['HTML','CSS'],
       link: 'https://darkphoenixninja92.github.io/Skilled-elearning-Landing-Page/',
    }
